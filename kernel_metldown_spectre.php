d0b89a87916d8e5889d397564e9bbef197f7337b
Jay Ramirez <jaywhs@gmail.com>
2018-03-05 09:05:02 -0800

    created a for each and added array to make future version inclusion easier

--- kernel_metldown_spectre.php
+++ kernel_metldown_spectre.php
@@ -1,51 +1,36 @@
 #!/usr/bin/php
 <?php
-//Written by Jay Ramirez
+//Owner: SRE
 //checks kernel type and runs a version check on kernel to see if it's on the patched update for Meltdown/Spectre
-//only checks amzn and centos type images
-$box_type = shell_exec("uname -r");
-
-if (strpos($box_type, 'amzn') !== false) {
-  kcheckamzn($box_type);
+ 
+//Approved minimum Operating System versions
+$OS["approved"] = (object) array(
+  "amzn"   => "4.9.81-35.56.amzn1.x86_64",
+  "el"     => "2.6.32-696.18.7.el6.x86_64",
+);
+ 
+$OS["current"] = shell_exec("uname -r");
+if (!isset($OS["current"])){
+  echo "WARN: Unable to retrieve kernel!\n";
+  exit(1);
 }
-elseif (strpos($box_type, 'centos') !== false) {
-  kcheckcentos($box_type);
-}
-else {
-  echo "Issue pulling image type";
-  exit (1);
-}
-
-//function for amazon boxes
-function kcheckamzn($version) {
-
-  if (version_compare("$version", "4.9.77-31.58.amzn1.x86_64") < 0) { //version stated is min version for aws
-    echo "WARN: I am still Kernel Version " . php_uname('r') . ".  I need to be 4.9.77-31.58.amzn1.x86_64 or greater\n";
-    exit (1);
-  }
-  elseif (version_compare("$version", "4.9.77-31.58.amzn1.x86_64") >= 0) {
-    echo "OK: I am at least Kernel Version 4.9.77-31.58.amzn1.x86_64, my version: " . php_uname('r') . "\n";
-    exit (0);
-  }
-  else {
-    echo "WARN: Issue pulling kernel version\n";
-    exit (1);
-  }
-}
-
-//function for centos boxes
-function kcheckcentos($version) {
-  if (version_compare("$version", "2.6.32-696.18.7.el6.centos.plus.x86_64") < 0) { //version stated is min version for centos
-    echo "WARN: I am still Kernel Version " . php_uname('r') . ".  I need to be 2.6.32-696.18.7.el6.centos.plus.x86_64 or greater\n";
-    exit (1);
-  }
-  elseif (version_compare("$version", "2.6.32-696.18.7.el6.centos.plus.x86_64") >= 0) {
-    echo "OK: I am at least Kernel Version 2.6.32-696.18.7.el6.centos.plus.x86_64, my version: " . php_uname('r') . "\n";
-    exit (0);
-  }
-  else {
-    echo "WARN: Issue pulling kernel version\n";
-    exit (1);
+ 
+foreach ($OS["approved"] as $name => $version)
+{
+  $OS["int_current"] = preg_replace("/[^0-9]/", "", $OS["current"]);
+  if (strpos($OS["current"], $name) !== false) {
+    //found the matching name - check version
+    $OS["int_compare"] = preg_replace("/[^0-9]/", "", $OS["approved"]->$name);
+    if ($OS["int_current"] >= $OS["int_compare"])
+    {
+      echo "OK: Kernel version is up to date: Current: " . $OS["current"]  . "Compared:" . $version  . "\n";
+      exit(0);
+    }
+    else {
+      echo "WARN: Kernel version is not up to date: Current: " . $OS["current"]  . "Compared:" . $version  . "\n";
+      exit(1);
+    }
   }
 }
+ 
 ?>
